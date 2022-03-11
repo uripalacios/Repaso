5b93661d1718fd194393a2bd6c03106410d2a022
uriel <uriel.palalo@educa.jcyl.es>
2022-03-11 16:03:32 +0000

    PR06Resuelto

--- PR06Arrays/codigo/liga.php
+++ PR06Arrays/codigo/liga.php
@@ -46,25 +46,37 @@ $liga =
             )
         ),
     );
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Array multidimensional y Asociativo</title>
+</head>
+<body>
 
-echo "<table border='1'><thead>";
+<?php
+echo "<table border='1'>";
             //guardo las cabeceras para despues poder compararlas
             $cabeceras=array();
 
             $blanco=0;
-            echo "<tr><th>Equipos</th>";
-            //visualizar la cabezera
+            echo "<tr><td class='equipos'>Equipos</td>";
+            //visualizar la cabecera
             foreach ($liga as $nombEquipoL => $nombEquipoV) {
-                echo "<th>".$nombEquipoL."</th>";
+                echo "<td class='nomb'>".$nombEquipoL."</td>";
                 $cabeceras[$blanco]=$nombEquipoL;
                 $blanco++;
             }
             echo"</tr>";
-echo"</thead><tbody>";
+
             $blanco=0;
             foreach ($liga as $nombEquipoL => $valueEquipoL) {
                 //barra lateral
-                echo "<tr><td>".$nombEquipoL."</td>";
+                echo "<tr><td class='nomb'>".$nombEquipoL."</td>";
              
                 foreach($valueEquipoL as $nombEquipoV=>$valueEquipoV){
                     if($cabeceras[$blanco]==$nombEquipoL){
@@ -74,16 +86,16 @@ echo"</thead><tbody>";
                     foreach ($valueEquipoV as $datos=>$valor) {
                         switch ($datos) {
                             case "Resultado":
-                                echo "<p>" .  $valor . "</p>";
+                                echo "<p><span class='verde'>" .  $valor . "<span></p>";
                                 break;
                             case "Roja":
-                                echo "<p>" .$valor;
+                                echo "<p><span class='rojo'>" .$valor."</span>";
                                 break;
                             case "Amarilla":
-                                echo $valor;
+                                echo "<span class='amarillo'>".$valor."</span>";
                                 break;
                             case "Penalti":
-                                echo $valor ."</p>";
+                                echo "<span class='naranja'>".$valor ."</span></p>";
                                 break;
                         }
                     
@@ -99,22 +111,76 @@ echo"</thead><tbody>";
             }
 
 
-echo "</tbody></table>";
+echo "</table>";
 //Tabla de clasificacion
 echo "<table border ='1'>";
 //cabecera de la tabla
 $thead=array('Equipos','Puntos','Goles a favor','Goles en contra');
-$puntos=0;
-$golFavor=0;
-$golContra=0;
 
+//array que contendra los valores de cada equipo
+$resultados=array();
+foreach ($cabeceras as $nombEquipoL) {
+    # code...
+    $resultados[$nombEquipoL]=array();
+    $resultados[$nombEquipoL]['Puntos']=0;
+    $resultados[$nombEquipoL]['GF']=0;
+    $resultados[$nombEquipoL]['GC']=0;
+}
+
+//comienzo a recorrer liga hasta llegar a resultado
+foreach ($liga as $nombEquipoL => $valueEquipoL) {
+    # code...
+    foreach ($valueEquipoL as $nombEquipoV => $valueEquipoV) {
+        # code...
+        foreach ($valueEquipoV as $datos => $value) {
+            # code...
+            if($datos=='Resultado'){
+                $result=explode('-',$value);
+                if($result[0]>$result[1]){
+                    //Victoria Local
+                    //asigno puntuacion a equipoL
+                    $resultados[$nombEquipoL]['Puntos']+=3;
+                    
+                }elseif($result[0]<$result[1]){
+                    //Victoria Visitante
+                    //asigno puntuacion a equipoV
+                    $resultados[$nombEquipoV]['Puntos']+=3;
+                }else{
+                    //Entraria en caso de empate
+                    //Asigno a los dos equipos
+                    $resultados[$nombEquipoL]['Puntos']+=1;
+                    $resultados[$nombEquipoV]['Puntos']+=1;
+                }
+                //Goles de cada equipo
+                //sumo los goles a favor a local
+                $resultados[$nombEquipoL]['GF']+=$result[0];
+                //sumo los goles en contra a local
+                $resultados[$nombEquipoL]['GC']+=$result[1];
+                //sumo los goles a favor a visitante
+                $resultados[$nombEquipoV]['GF']+=$result[1];
+                //sumo los goles en contra a visitante
+                $resultados[$nombEquipoV]['GC']+=$result[0];
+            }
+        }
+    }
+}
 //Comienzo de la tabla
 
 echo "<tr>";
 foreach ($thead as $texto) {
-    echo "<td>".$texto."</td>";
+    echo "<td class='nomb'>".$texto."</td>";
 }
 echo "</tr>";
-
-//contenido de la cabecera
+foreach ($resultados as $nombEquipoL => $valueEquipoL) {
+    # code...
+    echo "<tr><td class='nomb'>".$nombEquipoL."</td>";
+    foreach ($valueEquipoL as $key => $value) {
+        # code...
+        echo "<td>".$value."</td>";
+    }
+    echo"</tr>";
+}
+?>
+</body>
+</html>
 
